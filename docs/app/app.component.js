317e4a54176f4e2c6589d32fe1f1060b27139e7b
Meg <megunaokawa@gmail.com>
2017-01-30 19:45:51 +0900

    updated splash page

--- docs/app/app.component.js
+++ docs/app/app.component.js
@@ -9,17 +9,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-<<<<<<< HEAD
 var common_1 = require('@angular/common');
-=======
->>>>>>> updated js
 var AppComponent = (function () {
     function AppComponent(location) {
         this.title = 'Women in Japan 2017';
         this.hideNav = true;
         this.location = location;
     }
-<<<<<<< HEAD
     ;
     AppComponent = __decorate([
         core_1.Component({
@@ -29,16 +25,6 @@ var AppComponent = (function () {
             ]
         }), 
         __metadata('design:paramtypes', [common_1.Location])
-=======
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: 'my-app',
-            template: "\n  <div class=\"body-wrapper\">\n    <ul class=\"nav-wrapper flex-item\">\n        <li class=\"nav-item nav\"><a routerLink=\"/index\">Home </a> </li>\n        <li class=\"nav-item nav\"><a routerLink=\"/about\">About Us</a> </li>\n        <li class=\"nav-item nav\"><a routerLink=\"/speakers\">Speakers </a> </li>\n        <li class=\"nav-item nav\"><a routerLink=\"/register\">Register </a> </li>\n        <li class=\"nav-item nav\"><a routerLink=\"/contact\">Contact </a> </li>\n        <li class=\"nav-item nav\"><a routerLink=\"/faq\">FAQ </a> </li>\n      </ul>\n      <div class=\"content-wrapper flex-item\">\n        <router-outlet></router-outlet>\n      </div>\n    </div>\n  ",
-            styles: ["\n    .nav-item {\n      font-family: 'Nunito Sans', sans-serif;\n      display:inline-block;\n      padding:10px;\n      border-radius:5px;\n    }\n    .nav-item:hover {\n    }\n    a {\n      text-decoration:none;\n      color:#000;\n    }\n    a:hover {\n      color:#1ec9b0;\n      transition-duration:0.5s;\n    }\n    .nav-wrapper {\n      display:inline-block;\n      text-align:center;\n      border-bottom: 3px solid #000;\n      padding: 20px;\n      margin:0;\n      top:0;\n      left:0;\n      max-width:100vw;\n      background-color:rgba(0,0,0,0.2);\n    }\n    .body-wrapper {\n      display:flex;\n      flex-direction:column;\n      justify-content:center;\n    }\n    .content-wrapper {\n      text-align:center;\n      padding: 0 10vw 0 10vw;\n      max-width:100vw;\n      height:auto;\n    }\n    "
-            ]
-        }), 
-        __metadata('design:paramtypes', [])
->>>>>>> updated js
     ], AppComponent);
     return AppComponent;
 }());
---
+++ docs/app/splash.component.ts
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+=======
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'splash',
+  template: `
+    <div class="splash-wrapper">
+      <h1>WELCOME TO</h1>
+      <h1 class="main-title">WOMEN IN JAPAN 2017</h1>
+      <a class="splash-button" routerLink="/index">Enter Site \>\></a>
+    </div>
+  `,
+  styles:[`
+      .splash-button {
+        text-transform:uppercase;
+        padding:5px;
+        background-color:rgba(255, 255, 255, 0.3);
+        color:#343434;
+        font-size:2em;
+      }
+      .nav-wrapper {
+        display:none;
+      }
+      a.splash-button {
+        font-family: 'Unica One', system, sans-serif;
+      }
+      @media (min-width:600px) {
+        .splash-wrapper {
+          transform:translate(-50%,-50%);
+          position:absolute;
+          top:50vh;
+          left:50vw;
+        }
+      @media (max-width:600px) {
+        .splash-wrapper {
+          margin-top:10vh;
+        }
+      }
+    `]
+})
+export class SplashComponent {
+  public hideNav: boolean = true;
+}
+>>>>>>> updated splash page
