5d9bd6aae8fc1309c51b42d2c20b026acba16d9c
Meg <megunaokawa@gmail.com>
2017-01-30 19:45:51 +0900

    updated splash page

--- docs/app/splash.component.ts
+++ docs/app/splash.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'splash',
   template: `
-    <h1>WELCOME TO</h1>
-    <h1 class="main-title">WOMEN IN JAPAN 2017</h1>
-    <a class="splash-button" routerLink="/index">Enter Site</a>
+    <div class="splash-wrapper">
+      <h1>WELCOME TO</h1>
+      <h1 class="main-title">WOMEN IN JAPAN 2017</h1>
+      <a class="splash-button" routerLink="/index">Enter Site \>\></a>
+    </div>
   `,
   styles:[`
       .splash-button {
@@ -15,6 +17,26 @@ import { Component } from '@angular/core';
         color:#343434;
         font-size:2em;
       }
+      .nav-wrapper {
+        display:none;
+      }
+      a.splash-button {
+        font-family: 'Unica One', system, sans-serif;
+      }
+      @media (min-width:600px) {
+        .splash-wrapper {
+          transform:translate(-50%,-50%);
+          position:absolute;
+          top:50vh;
+          left:50vw;
+        }
+      @media (max-width:600px) {
+        .splash-wrapper {
+          margin-top:10vh;
+        }
+      }
     `]
 })
-export class SplashComponent { }
+export class SplashComponent {
+  public hideNav: boolean = true;
+}
