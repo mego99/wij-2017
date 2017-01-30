f83c57f1e214a53c779dd27963b00a84de71e3a3
Meg <megunaokawa@gmail.com>
2017-01-30 21:18:32 +0900

    merge conflict

--- app/schedule.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'schedule',
-  template: `
-    <h1>Schedule</h1>
-  `
-})
-export class ScheduleComponent { }
--- app/students.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'students',
-  template: `
-    <h1>Students</h1>
-  `
-})
-export class StudentsComponent { }
