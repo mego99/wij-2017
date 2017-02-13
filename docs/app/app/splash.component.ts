ec10b416a0841070bcfe8d2c0bf8c0c31c579179
Meg <megunaokawa@gmail.com>
2017-02-13 19:37:18 +0900

    updated splash page

--- docs/app/app/splash.component.ts
+++ docs/app/app/splash.component.ts
@@ -5,11 +5,28 @@ import { Component } from '@angular/core';
   template: `
     <div class="splash-wrapper">
       <h1>WELCOME TO</h1>
-      <h1 class="main-title">WOMEN IN JAPAN 2017</h1>
+      <h1 class="title">WOMEN IN JAPAN 2017</h1>
       <a class="splash-button" routerLink="/index">Enter Site \>\></a>
     </div>
+    <div class="logo"></div>
   `,
   styles:[`
+      .title {
+        top:5vh;
+      }
+      .logo {
+        background-image: url('../assets/logo-main.png');
+        background-repeat: no-repeat;
+        background-size:50vw;
+        height:50vw;
+        width:50vw;
+        position:fixed;
+        top:2vh;
+        left:50vw;
+        transform:translate(-50%,0);
+        z-index:-10;
+
+      }
       .splash-button {
         text-transform:uppercase;
         padding:5px;
@@ -30,9 +47,15 @@ import { Component } from '@angular/core';
           top:50vh;
           left:50vw;
         }
+        .title {
+          font-size:7em;
+        }
       @media (max-width:600px) {
         .splash-wrapper {
           margin-top:10vh;
+        }
+        .title {
+          font-size:3em;
         }
       }
     `]
