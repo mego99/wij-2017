4ad75d24d6a66c3b8ab65c9252f566d391bf7f86
Meg <megunaokawa@gmail.com>
2017-02-13 20:30:12 +0900

    updated information to match sage's

--- docs/app/app/about.component.ts
+++ docs/app/app/about.component.ts
@@ -5,10 +5,8 @@ import { Component } from '@angular/core';
   template: `
     <h1>About SAGE</h1>
     <p>The American School in Japan (ASIJ) Students Advocating for Gender Equality’s (SAGE) mission is to spread awareness about gender inequality issues all around the world, while also raising money for the Asian University for Women (AUW) in Bangladesh. Last year SAGE was able to raise enough money to support the scholarship of one young woman to the Asian University for Women. SAGE is sponsoring a conference on Tuesday, March 8, 2016 at The American School in Japan in Chofu, Tokyo. </p>
-    <h1>About ASIJ</h1>
-    <p>The American School in Japan (ASIJ) Students Advocating for Gender Equality’s (SAGE) mission is to spread awareness about gender inequality issues all around the world, while also raising money for the Asian University for Women (AUW) in Bangladesh. Last year SAGE was able to raise enough money to support the scholarship of one young woman to the Asian University for Women. SAGE is sponsoring a conference on Tuesday, March 8, 2016 at The American School in Japan in Chofu, Tokyo. </p>
     <h1>About AUW</h1>
-    <p>The American School in Japan (ASIJ) Students Advocating for Gender Equality’s (SAGE) mission is to spread awareness about gender inequality issues all around the world, while also raising money for the Asian University for Women (AUW) in Bangladesh. Last year SAGE was able to raise enough money to support the scholarship of one young woman to the Asian University for Women. SAGE is sponsoring a conference on Tuesday, March 8, 2016 at The American School in Japan in Chofu, Tokyo. </p>
+    <p>The American School in Japan (ASIJ) Students Advocating for Gender Equality’s (SAGE) mission is to spread awareness about gender inequality issues all around the world, while also raising money for the Asian University for Women (AUW) in Bangladesh. Last year SAGE was able to raise enough money to support the scholarship of two young women to the Asian University for Women. </p>
   `
 })
 export class AboutComponent {
