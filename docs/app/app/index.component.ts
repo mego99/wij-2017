405119f3e169d883733f64c9d093a25e0b2a0ba6
Meg <megunaokawa@gmail.com>
2017-02-23 00:07:35 +0900

    updated index page

--- docs/app/app/index.component.ts
+++ docs/app/app/index.component.ts
@@ -5,9 +5,29 @@ import { Component } from '@angular/core';
   template: `
     <h1 class="main-title">Women in Japan 2017</h1>
     <p>
-      This one-day conference (March 8th, 2017) for high school and university students will explore the current status of women in Japan. This conference is meant for high school and university students to explore the current status of women in Japan. Themed, “Problems, Possibilities, Power,” we will assess the current barriers facing women and explore the possibilities for future empowerment. We are honored to announce Fumiko Hayashi, Mayor of Yokohama, and Kuniko Inoguchi, House of Councillors Member, as our keynote speakers. 
+      This one-day conference (March 8th, 2017) for high school and university students will explore the current status of women in Japan. This conference is meant for high school and university students to explore the current status of women in Japan. Themed, “Problems, Possibilities, Power,” we will assess the current barriers facing women and explore the possibilities for future empowerment. We are honored to announce Fumiko Hayashi, Mayor of Yokohama, and Kuniko Inoguchi, House of Councillors Member, as our keynote speakers.
     </p>
-    <!-- <p class="japanese">このカンファレンス（2016年3月8日）のねらいは、高校生や大学生が日本の女性の現状を探ることです。参加者は現代の女性が抱える問題点を検討し、未来に向けての社会変革の機会を模索します。（ゲスト）スピーカーの方々は、日本の女性が直面している社会、政治、経済問題について参加者と話し合います。この度、光栄なことに基調講演に安倍昭恵さんをお迎えすることになります。</p> -->
-  `
+    <img class="bottom net" src="../assets/bottom.png"/>
+    <img class="top net" src="../assets/top.png"/>
+  `,
+  styles:[`
+    p {
+      background-color:rgba(255, 255, 255, 0.7);
+    }
+    .net {
+      width:60vw;
+      position:fixed;
+      z-index:-5;
+
+    }
+    .top {
+      right:2vw;
+      top:2vw;
+    }
+    .bottom {
+      left:2vw;
+      bottom:2vw;
+    }
+    `]
 })
 export class IndexComponent { }
