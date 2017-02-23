1a5468f9bd7f64a34a515eeb7dd080f9e2fd5971
Meg <megunaokawa@gmail.com>
2017-02-23 19:25:23 +0900

    fixed splash page to work with mobile

--- docs/app/app/splash.component.ts
+++ docs/app/app/splash.component.ts
@@ -4,21 +4,28 @@ import { Component } from '@angular/core';
   selector: 'splash',
   template: `
     <div class="splash-wrapper">
-      <h1>WELCOME TO</h1>
-      <h1 class="title">WOMEN IN JAPAN 2017</h1>
+      <h3 class="welcome">WELCOME TO</h3>
+      <h3 class="title">WOMEN IN JAPAN 2017</h3>
       <a class="splash-button" routerLink="/index">Enter Site \>\></a>
     </div>
     <div class="logo"></div>
   `,
   styles:[`
-      h1 {
-        box-shadow:none;
+      h3 {
+        font-family: 'Unica One', sans-serif;
+        margin:0;
+      }
+      .welcome {
+        font-size: 2.5em;
       }
       .background {
         /*background-color: #ed2024;*/
       }
       .title {
+
         top:5vh;
+        margin-top:0.2vh;
+        margin-bottom:0.2vh;
       }
       .logo {
         background-image: url('../assets/logo-main.png');
@@ -35,10 +42,10 @@ import { Component } from '@angular/core';
       }
       .splash-button {
         text-transform:uppercase;
-        padding:5px;
         background-color:rgba(255, 255, 255, 0.3);
         color:#343434;
         font-size:2em;
+
       }
       .nav-wrapper {
         display:none;
@@ -46,22 +53,38 @@ import { Component } from '@angular/core';
       a.splash-button {
         font-family: 'Unica One', system, sans-serif;
       }
-      @media (min-width:600px) {
+
+      .splash-wrapper {
+        top:10vh;
+        position:absolute;
+        margin:auto;
+        left:50vw;
+        transform:translateX(-50%);
+      }
+
+      @media only screen and (min-width:600px) {
         .splash-wrapper {
           transform:translate(-50%,-50%);
           position:absolute;
-          top:50vh;
+          top:40vh;
           left:50vw;
         }
         .title {
           font-size:7em;
         }
-      @media (max-width:600px) {
+        ,splash-wrapper {
+          display:flex;
+        }
+      @media only screen and (max-width:600px) {
         .splash-wrapper {
-          margin-top:10vh;
+          top:20vh;
+          position:absolute;
         }
         .title {
           font-size:3em;
+        }
+        h3 {
+          font-size:2em;
         }
       }
     `]
